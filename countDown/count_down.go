f4bb0080913822578dc7a3398f6583cbd594460d
yisheng <58337938@qq.com>
2022-08-10 21:04:11 +0800

    countDown

---
+++ countDown/count_down.go
@@ -0,0 +1,32 @@
+package countDown
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) sleep() {
+	s.Calls++
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
+		fmt.Fprintln(out, i)
+	}
+
+	time.Sleep(1 * time.Second)
+	fmt.Fprint(out, finalWord)
+}
